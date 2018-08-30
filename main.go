34426bf0daf7fd8b8abb25d2830d44b2186bfb46
hyetpang <hyetpang@yeah.net>
2018-08-30 18:55:59 +0800

    增加字体以及字体大小的配置

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
 	"github.com/go-ini/ini"
 	"github.com/golang/freetype"
+	"github.com/golang/freetype/truetype"
 	"golang.org/x/image/font"
 	"image"
 	"image/color"
@@ -19,25 +19,26 @@ import (
 )
 
 var (
-	dpi      = flag.Float64("dpi", 72, "screen resolution in Dots Per Inch")
-	fontfile = flag.String("fontfile", "C:\\Windows\\Fonts\\simkai.ttf", "filename of the ttf font")
-	hinting  = flag.String("hinting", "none", "none | full")
-	size     = flag.Float64("size", 24, "font size in points")
-	spacing  = flag.Float64("spacing", 1.5, "line spacing (e.g. 2 means double spaced)")
-	wonb     = flag.Bool("whiteonblack", false, "white text on a black background")
+	globalFontPath string
+	globalFontSize float64
 )
 
 const (
+	dpi                 = 72
 	dataFile            = "my.ini"
 	textSection         = "text"
 	textWords           = "words"
 	textSeparator       = ","
 	iniPackageSeparator = "."
+	chapterOfPrincipal  = "校长的章"
+	imageSection        = "image"
+	fontFilePath        = "C:\\Windows\\Fonts\\"
 )
 
 type Text struct {
-	Content string
-	X, Y    int
+	Content, FontName string
+	X, Y              int
+	FontSize          float64
 }
 
 func main() {
@@ -52,37 +53,36 @@ func main() {
 	// 打开需要画的图片
 	backgroundImageFile, err := os.Open("1.jpg")
 	if err != nil {
-		fmt.Println("打开模板文件失败:%v", err)
+		fmt.Printf("打开模板文件失败:%v\r\n", err)
 		os.Exit(1)
 	}
 	backgroundImage, err := jpeg.Decode(backgroundImageFile)
 	if err != nil {
-		fmt.Println("解码模板文件失败:%v", err)
+		fmt.Printf("解码模板文件失败:%v\r\n", err)
 		os.Exit(1)
 	}
 	backgroundImageBounds := backgroundImage.Bounds()
 	newBackgroundImage := image.NewRGBA(backgroundImageBounds)
 	// 把模板文件画到新的图片文件上
-	fontBytes, err := ioutil.ReadFile(*fontfile)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	f, err := freetype.ParseFont(fontBytes)
-	if err != nil {
-		log.Println(err)
-		return
-	}
 	draw.Draw(newBackgroundImage, backgroundImageBounds, backgroundImage, image.ZP, draw.Src)
 	textSectionData := data.Section(textSection)
 	c := freetype.NewContext()
-	c.SetDPI(*dpi)
-	c.SetFont(f)
-	c.SetFontSize(*size)
+	c.SetDPI(dpi)
 	c.SetClip(backgroundImageBounds)
 	c.SetDst(newBackgroundImage)
 	c.SetSrc(image.Black)
 	c.SetHinting(font.HintingNone)
+	// 先获取全局的字体和字体大小
+	globalFontFile := textSectionData.Key("字体").String()
+	globalFontPath = fontFilePath + globalFontFile
+	if err != nil {
+		panic("全局字体不存在")
+	}
+	globaleFontSizeStr := textSectionData.Key("字体大小").String()
+	globalFontSize, err = strconv.ParseFloat(globaleFontSizeStr, 64)
+	if err != nil {
+		panic("全局字体大小请输入数字!")
+	}
 	// 画文本
 	for _, key := range textSectionData.KeyStrings() {
 		if key == textWords {
@@ -90,11 +90,9 @@ func main() {
 			textWordsData := strings.Split(strings.Replace(textSectionValue, "，", textSeparator, -1), textSeparator)
 			for _, word := range textWordsData {
 				textData := data.Section(textSection + iniPackageSeparator + word)
-				fmt.Printf("Section:%v\r\n", textData.Name())
 				keyStrings := textData.KeyStrings()
 				imageText := Text{}
 				for _, key := range keyStrings {
-					fmt.Printf("key:%v, value:%v\r\n", key, textData.Key(key).String())
 					// 画出文字
 					switch key {
 					case word:
@@ -103,72 +101,87 @@ func main() {
 						position := strings.Split(textData.Key(key).String(), ",")
 						imageText.X, err = strconv.Atoi(position[0])
 						if err != nil {
-							fmt.Println("输入的位置不是数字!:%v", err)
+							fmt.Printf("输入的位置不是数字!:%v\r\n", err)
 							os.Exit(1)
 						}
 						imageText.Y, err = strconv.Atoi(position[1])
 						if err != nil {
-							fmt.Println("输入的位置不是数字!:%v", err)
+							fmt.Printf("输入的位置不是数字!:%v\r\n", err)
 							os.Exit(1)
 						}
+					case "字体":
+						imageText.FontName = textData.Key(key).String()
+					case "字体大小":
+						fontSizeStr := textData.Key(key).String()
+						fontSize, err := strconv.ParseFloat(fontSizeStr, 64)
+						if err != nil {
+							panic("字体大小请输入数字!")
+						}
+						imageText.FontSize = fontSize
 					}
 				}
 				if imageText.Content == "" && imageText.X == 0 && imageText.Y == 0 {
 					continue
 				}
-				pt := freetype.Pt(imageText.X, imageText.Y)
-				p, err := c.DrawString(imageText.Content, pt)
+				fontSize := imageText.FontSize
+				if fontSize == 0.00 {
+					fontSize = globalFontSize
+				}
+				fontFile := imageText.FontName
+				if fontFile == "" {
+					fontFile = globalFontPath
+				} else {
+					fontFile = fontFilePath + imageText.FontName
+				}
+				imageFont, err := getFont(fontFile)
+				// 设置字体
 				if err != nil {
-					fmt.Println("画文本失败!:%v", err)
+					panic(fmt.Sprintf("读取字体%q文件出错:%v", fontFilePath+imageText.FontName, err))
+				}
+				c.SetFont(imageFont)
+				c.SetFontSize(fontSize)
+				pt := freetype.Pt(imageText.X, imageText.Y)
+				_, err = c.DrawString(imageText.Content, pt)
+				if err != nil {
+					fmt.Printf("画文本失败!:%v\r\n", err)
 					os.Exit(1)
 				}
-				fmt.Printf("%#v\r\n", p)
 			}
 		}
 	}
 	// 画圆
-	for i := 0; i < 10; i++ {
-		if i < 5 {
-			drawCircle(newBackgroundImage, 580, 610, 90+i, color.RGBA{R: 189, G: 59, B: 25, A: 0xff}, c)
-		} else {
-			//drawCircle(newBackgroundImage, 580, 610, 90+i-5, color.RGBA{R:170, G:76, B:51})
-		}
-	}
+	//for i := 0; i < 4; i++ {
+	//	drawCircle(newBackgroundImage, 580, 610, 90+i, color.RGBA{R: 189, G: 59, B: 25, A: 0xff}, c)
+	//}
+	chapter := data.Section(imageSection + "." + chapterOfPrincipal)
 	// 校长的章
-	//for i:=750; i<800; i++ {
-	//	newBackgroundImage.Set(i+1, 540, colornames.Red)
-	//	newBackgroundImage.Set(i+1, 540+1, colornames.Red)
-	//}
-	//for i:=540; i<650; i++ {
-	//	newBackgroundImage.Set(750, i, colornames.Red)
-	//	newBackgroundImage.Set(750+1, i, colornames.Red)
-	//}
-	//for i:=750; i<800; i++ {
-	//	newBackgroundImage.Set(i+1, 649, colornames.Red)
-	//	newBackgroundImage.Set(i+1, 650, colornames.Red)
-	//}
-	//for i:=540; i<650; i++ {
-	//	newBackgroundImage.Set(800, i, colornames.Red)
-	//	newBackgroundImage.Set(800+1, i, colornames.Red)
-	//}
-	drawRect(newBackgroundImage, image.Point{X: 760, Y: 570}, image.Point{X: 830, Y: 640}, color.RGBA{R: 157, G: 85, B: 50, A: 0xff}, c)
+	drawRect(newBackgroundImage, image.Point{X: 760, Y: 570}, image.Point{X: 830, Y: 640}, color.RGBA{R: 157, G: 85, B: 50, A: 0xff}, c, chapter)
 	// 保存
 	saveImage, err := os.Create("a.jpg")
 	if err != nil {
-		fmt.Println("创建文件失败:%v", err)
+		fmt.Printf("创建文件失败:%v\r\n", err)
 		os.Exit(1)
 	}
 	defer saveImage.Close()
 	saveImageWriter := bufio.NewWriter(saveImage)
 	err = jpeg.Encode(saveImageWriter, newBackgroundImage, &jpeg.Options{Quality: 75})
 	if err != nil {
-		fmt.Println("保存文件失败:%v", err)
+		fmt.Printf("保存文件失败:%v\r\n", err)
 		os.Exit(1)
 	}
+	fmt.Println("success!")
+}
+func getFont(fontFilePath string) (*truetype.Font, error) {
+	fontBytes, err := ioutil.ReadFile(fontFilePath)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	return freetype.ParseFont(fontBytes)
 }
 
 // 画校长的章
-func drawRect(img draw.Image, p1 image.Point, p2 image.Point, col color.Color, context *freetype.Context) {
+func drawRect(img draw.Image, p1 image.Point, p2 image.Point, col color.Color, context *freetype.Context, chapterSection *ini.Section) {
 	// 先画横的
 	x := p1.X
 	x1 := p2.X
@@ -199,21 +212,18 @@ func drawRect(img draw.Image, p1 image.Point, p2 image.Point, col color.Color, c
 	}
 	// 画字
 	newPoint1 := image.Point{X: p1.X - 5, Y: p1.Y - 5}
-	//newPoint2 := image.Point{X: p2.X-5, Y: p2.Y-5}
-	//context.SetSrc(image.NewUniform(color.RGBA{R:230, G:190, B:150}))
 	context.SetSrc(image.NewUniform(color.RGBA{R: 187, G: 80, B: 60, A: 0xff}))
 	context.SetFontSize(35)
-
-	context.DrawString("陈", freetype.Pt(newPoint1.X+39, newPoint1.Y+40))
-	context.DrawString("中", freetype.Pt(newPoint1.X+39, newPoint1.Y+70))
-	context.DrawString("江", freetype.Pt(newPoint1.X+7, newPoint1.Y+40))
-	context.DrawString("印", freetype.Pt(newPoint1.X+7, newPoint1.Y+70))
+	names := strings.Split(chapterSection.Key("校长的章").String(), "")
+	context.DrawString(names[0], freetype.Pt(newPoint1.X+39, newPoint1.Y+40))
+	context.DrawString(names[1], freetype.Pt(newPoint1.X+39, newPoint1.Y+70))
+	context.DrawString(names[2], freetype.Pt(newPoint1.X+7, newPoint1.Y+40))
+	context.DrawString(names[3], freetype.Pt(newPoint1.X+7, newPoint1.Y+70))
 }
 
 func drawCircle(img draw.Image, x0, y0, r int, c color.Color, context *freetype.Context) {
 	x, y, dx, dy := r-1, 0, 1, 1
 	err := dx - (r * 2)
-
 	for x > y {
 		img.Set(x0+x, y0+y, c)
 		img.Set(x0+y, y0+x, c)
